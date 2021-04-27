92eeb7cd9d4bab90298e123b3724239c93f36935
Gautier <gautier@nexus.ethz.ch>
2021-04-27 17:41:49 +0200

    got back my notes folder

--- covmass/admin.py
+++ covmass/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Test
+from .models import Zone, Infected, Deceased, Metric
 
-admin.site.register(Test)
+admin.site.register(Zone)
+admin.site.register(Infected)
+admin.site.register(Deceased)
+admin.site.register(Metric)
--- covmass/views.py
+++ covmass/views.py
@@ -4,5 +4,4 @@ from .tasks import *
 # Create your views here.
 
 def index(request):
-  sleepy(2)
   return render(request, "covmass/index.html", {})
