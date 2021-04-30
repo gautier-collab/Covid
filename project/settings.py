125f8668d9007160d10b8e6cf6f580ad72f826f0
Gautier <gautier@nexus.ethz.ch>
2021-04-30 02:00:05 +0200

    modified requirements

--- project/settings.py
+++ project/settings.py
@@ -11,9 +11,8 @@ load_dotenv(dotenv_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
