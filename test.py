4fec556d7f38fadc86674217e3a92c89db4cc60a
Gautier <gautier@nexus.ethz.ch>
2021-04-30 04:20:27 +0200

    changed test.py configuration for heroku

--- test.py
+++ test.py
@@ -6,8 +6,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
-driver = webdriver.Chrome(PATH)
+
+
+
+
+
+
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+
+
+
+
+
+
+
+# PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
+# driver = webdriver.Chrome(PATH)
 
 driver.get("https://covid19.who.int")
 
