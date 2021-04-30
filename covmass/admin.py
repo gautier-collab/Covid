e43ea95a13324f3794ca84732c8d6ab1390555d5
Gautier <gautier@nexus.ethz.ch>
2021-04-30 17:41:36 +0200

    save before backup

--- covmass/admin.py
+++ covmass/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from .models import Zone, Infected, Deceased, Source, Update, Metric
 
-# class InfectedInline(admin.TabularInline):
-#   model = Infected
+class InfectedInline(admin.TabularInline):
+  model = Infected
 
-# class MetricAdmin(admin.ModelAdmin):
-#   readonly_fields = ('update',)
-#   inlines = [
-#     InfectedInline,
-#   ]
+class MetricAdmin(admin.ModelAdmin):
+  readonly_fields = ('update',)
+  inlines = [
+    InfectedInline,
+  ]
 
 admin.site.register(Update)
 admin.site.register(Zone)
 admin.site.register(Infected)
 admin.site.register(Deceased)
 admin.site.register(Source)
-# admin.site.register(Metrick, MetricAdmin)
+admin.site.register(Metric, MetricAdmin)
