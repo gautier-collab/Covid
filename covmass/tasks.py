591bd50d1aaf013a7d2d74be114c17124c595f6c
Gautier <gautier@nexus.ethz.ch>
2021-04-30 13:34:16 +0200

    print innerHTML

--- covmass/tasks.py
+++ covmass/tasks.py
@@ -99,6 +99,7 @@ def ncov_scrape(driver):
 
     # update DBvalues for infected
     temp = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".text--green.text--green.sorting_1")))
+    print("ROW TEXT:\n" + row.get_attribute('innerHTML'))
     total_infected = number(cell_val(row, ".text--green.text--green.sorting_1"))
     print(location + " total infected : " + str(total_infected))
     infected = Infected.objects.get(zone=Zone.objects.get(name=location))
@@ -133,18 +134,18 @@ def scrape():
 
   print(f"Execution start: {datetime.datetime.now()}")
 
-  # # Dev mode config
-  # PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
-  # driver = webdriver.Chrome(PATH)
+  # Dev mode config
+  PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
+  driver = webdriver.Chrome(PATH)
 
-  # Prod mode config
-  import os
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-  chrome_options.add_argument("--headless")
-  chrome_options.add_argument("--disable-dev-shm-usage")
-  chrome_options.add_argument("--no-sandbox")
-  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  # # Prod mode config
+  # import os
+  # chrome_options = webdriver.ChromeOptions()
+  # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  # chrome_options.add_argument("--headless")
+  # chrome_options.add_argument("--disable-dev-shm-usage")
+  # chrome_options.add_argument("--no-sandbox")
+  # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
   WHO_scrape(driver)
   ncov_scrape(driver)
