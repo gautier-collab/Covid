0f636cbf560c7d9eec85d5f61915b01f8a0621ed
Gautier <gautier@nexus.ethz.ch>
2021-04-30 13:39:03 +0200

    prod

--- covmass/tasks.py
+++ covmass/tasks.py
@@ -134,18 +134,18 @@ def scrape():
 
   print(f"Execution start: {datetime.datetime.now()}")
 
-  # Dev mode config
-  PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
-  driver = webdriver.Chrome(PATH)
+  # # Dev mode config
+  # PATH="/Users/gautier/Documents/Z/Chromedriver/chromedriver"
+  # driver = webdriver.Chrome(PATH)
 
-  # # Prod mode config
-  # import os
-  # chrome_options = webdriver.ChromeOptions()
-  # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-  # chrome_options.add_argument("--headless")
-  # chrome_options.add_argument("--disable-dev-shm-usage")
-  # chrome_options.add_argument("--no-sandbox")
-  # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  # Prod mode config
+  import os
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
   WHO_scrape(driver)
   ncov_scrape(driver)
