d44860e80d1a0e338bee512d5b5830bb5a2436c3
Gautier <gautier@nexus.ethz.ch>
2021-05-05 14:28:39 +0200

    img source back to normal

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,6 @@ load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 # STATIC_ROOT = "/static/"
