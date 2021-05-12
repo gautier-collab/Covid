3250ce4c0596157224097b4b46a9f7d9295d10c9
Gautier <gautier@nexus.ethz.ch>
2021-05-12 09:28:29 +0200

    changed name of file to download

--- covmass/views.py
+++ covmass/views.py
@@ -1,3 +1,7 @@
+import os
+from os import walk
+from datetime import datetime
+from django.conf import settings
 from django.shortcuts import render
 from .tasks import *
 from .models import Deceased, Infected, Zone, Metric
@@ -5,32 +9,22 @@ from .models import Deceased, Infected, Zone, Metric
 
 def index(request):
   
+  path=f"{settings.BASE_DIR}/static_cdn/media_root"
+  new_name = f"{datetime.today().year}{str(datetime.today().month).zfill(2)}{str(datetime.today().day).zfill(2)}-{str(datetime.today().hour).zfill(2)}{str(datetime.today().minute).zfill(2)}.covid-lagebericht.docx"
+  for (dirpath, dirnames, filenames) in walk(path):
+    for filename in filenames:
+      if "lagebericht" in filename:
+        current_name = filename
+        os.rename(f'{path}/{current_name}',f'{path}/{new_name}')
+  
   context = {
   "deceased": Deceased.objects.all(),
   "infected": Infected.objects.all(),
   "zones": Zone.objects.all(),
   "metrics": Metric.objects.all(),
-  "update": Update.objects.all().last()
+  "update": Update.objects.all().last(),
+  "filename": new_name
   }
 
   return render(request, "covmass/index.html", context)
 
-
-# from django.core.files.storage import FileSystemStorage
-
-# def uploadfile_view(request):
-#   if request.method == 'POST':
-#     f = request.FILES['file']
-#     fs = FileSystemStorage()
-#     filename, ext=str(f).split('.')
-#     file = fs.save(str(f),f)
-#     fileurl = fs.url(file)
-#     size = fs.size(file)
-#     return render(request, 'covmass/uploadfile.html', {
-#       'fileUrl':fileurl, 
-#       "fileName": filename,
-#       "ext":ext, 
-#       "size": size,
-#     })
-#   else:
-#     return render(request, 'covmass/uploadfile.html', {})
