a62c32a153223e40cfa25490777f9748a4225a3f
Gautier <gautier@nexus.ethz.ch>
2021-05-14 13:50:35 +0200

    cron back to normal

--- covid/cron.py
+++ covid/cron.py
@@ -3,11 +3,11 @@ from covid.tasks import scrape
 
 def cron(hour, minute):
 
-  # schedule.every().day.at(f"{hour}:{minute}").do(scrape)
+  schedule.every().day.at(f"{hour}:{minute}").do(scrape)
 
-  # while True:
-  #   schedule.run_pending()
-  #   time.sleep(60) # wait one minute
+  while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
 
-  # Execute immediately for test:
-  scrape()
+  # # Execute immediately for test:
+  # scrape()
